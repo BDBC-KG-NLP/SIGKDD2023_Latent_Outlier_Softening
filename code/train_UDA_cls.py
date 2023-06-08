5f1089716bf275153fbd2d80c6ab2362067d27f6
cjc <1311345439@qq.com>
2023-06-08 17:40:30 +0800

    cls

--- code/train_UDA_cls.py
+++ code/train_UDA_cls.py
@@ -529,7 +529,7 @@ def test_out_of_distribution_set(testloader, model, encoder, classifier, criteri
         if args.n_labeled == 10:
             threshold = 0.6
         elif args.n_labeled == 50:
-            threshold = 0.75
+            threshold = 0.8
         else:
             threshold = 0.85
         print(args.n_labeled)
@@ -575,7 +575,7 @@ def test_all_set(testloader, model, encoder, classifier, criterion, index_select
         if args.n_labeled == 10:
             threshold = 0.6
         elif args.n_labeled == 50:
-            threshold = 0.75
+            threshold = 0.8
         else:
             threshold = 0.85
         print(args.n_labeled)
